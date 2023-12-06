6eb85d49038ae243afee16cd9af72d139795a27c
JulienLach <leadedchamps@gmail.com>
2023-12-06 14:25:33 +0100

    ok

---
+++ script.py
@@ -0,0 +1,14 @@
+# Simple Python Script
+
+name = input("Enter your name: ")
+
+age = input("Enter your age: ")
+
+age = int(age)
+
+print(f"Hello, {name}! You are {age} years old.")
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
